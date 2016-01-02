31e28f66379f10f8aa00c488c5c636c280ff696f
Neil Sarkar <neil@gotinder.com>
2016-01-02 01:24:12 -0500

    Checkpoint: parse class details properly

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.get('/test', function(req, res) {
+  res.sendFile(__dirname + '/test.html');
+})
+
+app.listen(3000, function(err) {
+  if( err ) { throw err; }
+})
--- phantom.js
+++ phantom.js
@@ -1,54 +1,39 @@
 var page = require('webpage').create();
 
 page.onError = function(msg, trace) {
-  var msgStack = ['ERROR: ' + msg];
-
-  if (trace && trace.length) {
-   msgStack.push('TRACE:');
-   trace.forEach(function(t) {
-     msgStack.push(' -> ' + t.file + ': ' + t.line + (t.function ? ' (in function "' + t.function +'")' : ''));
-   });
-  }
-
-  console.error(msgStack.join('\n'));
-
+  throw "Page error " + msg;
   phantom.exit();
 }
 
 phantom.onError = function(msg, trace) {
-  var msgStack = ['PHANTOM ERROR: ' + msg];
-
-  if (trace && trace.length) {
-   msgStack.push('TRACE:');
-   trace.forEach(function(t) {
-     msgStack.push(' -> ' + t.file + ': ' + t.line + (t.function ? ' (in function "' + t.function +'")' : ''));
-   });
-  }
-
-  console.error(msgStack.join('\n'));
-
+  throw "Phantom error " + msg;
   phantom.exit();
 }
 
-page.open('http://localhost:3000/test', function(status) {
-// page.open('https://losangeles.ucbtrainingcenter.com/course/open', function(status) {
+var url = 'https://losangeles.ucbtrainingcenter.com/course/open';
+url = 'http://localhost:3000/test';
+
+page.open(url, function(status) {
   if (status !== 'success') {
     return console.error('Unable to access network');
   }
 
-
-
-  var classes = page.evaluate(function(s) {
-    return document.querySelectorAll(s)
+  var elements = page.evaluate(function(s) {
+    return [].map.call(document.querySelectorAll(s), function(el) {
+      return {
+        level: el.querySelector('td.widget-text').innerHTML.trim(),
+        time: el.querySelector('td:nth-child(2)').innerHTML.trim(),
+        start_date: el.querySelector('td:nth-child(3)').innerHTML.trim(),
+        teacher: el.querySelector('td:nth-child(4)').innerHTML.trim(),
+        sold_out: !!el.querySelector('a.btn-danger'),
+        registration_link: el.querySelector('a').href
+      }
+    });
   }, '.panel-group:nth-child(4) > .panel table tbody tr');
 
-  console.log(classes[1].innerHTML);
-  // console.log(classes[1].innerHTML);
-  // console.log(classes[2].innerHTML);
-  // console.log(classes[3].innerHTML);
-  // for( var i = 0; i < classes.length; i++ ) {
-  //   console.log(i);
-  //   // console.log(classes[i].innerHTML);
-  // }
+  for( var i = 0; i < elements.length; i++ ) {
+    console.log(JSON.stringify(elements[i]));
+  }
+
   phantom.exit();
 });
